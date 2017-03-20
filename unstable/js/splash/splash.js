edc46847d91ebb41380bc1bb058d6032d23580fe
Garrett <garrett@heywordboy.com>
2017-03-20 11:45:09 -0700

    removed slide over

--- unstable/js/splash/splash.js
+++ unstable/js/splash/splash.js
@@ -155,7 +155,7 @@ SplashScene.prototype.update = function(delta_ms) {
 		this.flash_g.alpha = interp_clamp(scene.scene_time, 8520, 8820, 1, 0);
 		this.undyne_sprite.position.y = 360 - 1080 * interp_clamp(scene.scene_time, 8520, 9000, 0, 1);
 		//this.un_sprite.position.y = -780 + 1060 * Math.pow(interp_clamp(scene.scene_time, 9000, 10500, 0, 1), 2);
-	} else if (scene.scene_time < 12000) {
+	} else if (scene.scene_time < 11000) {
 		if (this.undyne_jumped == false) {
 			this.undyne_jumped = true;
 			splash_stage.removeChild(this.undyne_sprite);
@@ -168,7 +168,7 @@ SplashScene.prototype.update = function(delta_ms) {
 		/*this.un_sprite.position.y = -780 + 1060 * Math.pow(interp_clamp(scene.scene_time, 9000, 10500, 0, 1), 2);
 		this.un_sprite.position.x = 140 + 20 * Math.pow(interp_clamp(scene.scene_time, 10500, 10700, 1, 0), 2);
 		this.un_sprite.rotation = -0.3 * (1 - Math.pow(interp_clamp(scene.scene_time, 10500, 10900, -1, 1), 4));*/
-		this.fairdyne_sprite.position.x = 400 - 80 * Math.pow(interp_clamp(scene.scene_time, 10500, 10700, 1, 0), 2);
+		//this.fairdyne_sprite.position.x = 400 - 80 * Math.pow(interp_clamp(scene.scene_time, 10500, 10700, 1, 0), 2);
 		this.undyne_sprite.position.y = 160 - 400 * interp_clamp(scene.scene_time, 11400, 12000, 1, 0);
 		this.undyne_hair_sprite.position.y = 60 - 400 * interp_clamp(scene.scene_time, 11400, 12000, 1, 0);
 	} else {
